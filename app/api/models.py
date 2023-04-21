34042e792c3ea72f92934d6fce4846058469316a
Mgalazyn <m.galazynn@gmail.com>
2023-04-21 19:57:55 +0200

    creating models, setting admin panel

--- app/api/models.py
+++ app/api/models.py
@@ -1,3 +1,36 @@
 from django.db import models
-
+from django.contrib.auth.models import (AbstractBaseUser,
+                                        BaseUserManager,
+                                        PermissionsMixin)
+from django.core.validators import validate_email
 # Create your models here.
+
+
+class UserManager(BaseUserManager):
+    #Method for creating users 
+    def create_user(self, email, password=None, **extra_fields):
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(max_length=250, unique=True, blank=False, null=False, validators=[validate_email])
+    name = models.CharField(max_length=250)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.email
